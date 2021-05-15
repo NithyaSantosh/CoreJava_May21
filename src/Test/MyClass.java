fbc0ff78ca86931972105471ffac0cf170d18dec
Nithya <nithya.santosh5@gmail.com>
2021-05-15 18:56:13 +0000

    My first commit

---
+++ src/Test/MyClass.java
@@ -0,0 +1,22 @@
+package Test;
+
+public class MyClass {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+		
+		// data types in Java
+		
+		int num1;
+		int num2;
+		
+		num1=10;
+		num2=10;
+		
+		int Sum=num1+num2;
+		System.out.println(Sum);
+	}
+	                                        
+	                                       
+
+}
