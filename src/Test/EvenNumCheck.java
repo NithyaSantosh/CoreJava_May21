608dc88dfcc9b101799b137cedd8cff5ccdec9a9
Nithya <nithya.santosh5@gmail.com>
2021-05-15 19:18:46 +0000

    EvenNumberCheck

---
+++ src/Test/EvenNumCheck.java
@@ -0,0 +1,27 @@
+package Test;
+
+import java.util.Scanner;
+
+public class EvenNumCheck {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number : ");
+        int number = scanner.nextInt();
+        
+        if (number%2 ==0)
+        {
+            System.out.println("Number is even : " + number);
+        }
+        
+        else 
+        {
+            System.out.println("Number is not even : " + number);
+        }
+        
+
+	}
+
+}
